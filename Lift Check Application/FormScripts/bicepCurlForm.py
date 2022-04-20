c640a2b436b66245eda05436923a6f35c491e167
michaelwitusikk <michaelwitusik@email.arizona.edu>
2022-04-20 02:12:43 -0700

    v 2.0

--- Lift Check Application/FormScripts/bicepCurlForm.py
+++ Lift Check Application/FormScripts/bicepCurlForm.py
@@ -10,10 +10,12 @@ from FormScripts import workoutSetClass
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-def bicepRendering(max_reps,draw_pose):
+
+def bicepRendering(max_reps, draw_pose):
 
     s = workoutSetClass.workout_set()
-    rep_count = 0
+    Lrep_count = 0
+    Rrep_count = 0
     count = 90
     tracking = None
     currentLeftRep = None
@@ -35,26 +37,32 @@ def bicepRendering(max_reps,draw_pose):
             # Render detections
             if draw_pose:
                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                        mp_drawing.DrawingSpec(
-                                            color=(245, 117, 66), thickness=2, circle_radius=2),
-                                        mp_drawing.DrawingSpec(
-                                            color=(245, 66, 230), thickness=2, circle_radius=2)
-                                        )
-            
-            
-            #find the angle of the elbows and the angle of the shoulders
+                                          mp_drawing.DrawingSpec(
+                                              color=(245, 117, 66), thickness=2, circle_radius=2),
+                                          mp_drawing.DrawingSpec(
+                                              color=(245, 66, 230), thickness=2, circle_radius=2)
+                                          )
+
+            # find the angle of the elbows and the angle of the shoulders
             try:
                 left_angle = calculate_left_arm(results)
                 right_angle = calculate_right_arm(results)
                 left_armpit_angle = calculate_left_armpit(results)
                 right_armpit_angle = calculate_right_armpit(results)
-                #display the angle on the screen
-                cv2.putText(image, "Left Bicep: " + str(left_angle), (15, 35), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
-                cv2.putText(image, "Right Bicep: " + str(right_angle), (355, 35), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
-                cv2.putText(image, "right armpit: " + str(right_armpit_angle), (355, 65), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
-                cv2.putText(image, "left armpit: " + str(left_armpit_angle), (15, 65), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)  
-                
-                
+                # display the angle on the screen
+                cv2.putText(image, "Left Bicep: " + str(left_angle),
+                            (15, 35), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+                cv2.putText(image, "Right Bicep: " + str(right_angle),
+                            (355, 35), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+                cv2.putText(image, "right armpit: " + str(right_armpit_angle),
+                            (355, 65), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+                cv2.putText(image, "left armpit: " + str(left_armpit_angle),
+                            (15, 65), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+                cv2.putText(image, "Right Count: " + str(Rrep_count),
+                            (355, 95), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+                cv2.putText(image, "Left Count: " + str(Lrep_count),
+                            (15, 95), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+
                 # Are we tracking yet?
                 if tracking:
 
@@ -63,7 +71,7 @@ def bicepRendering(max_reps,draw_pose):
                             currentLeftRep = repClass.rep()
                             currentLeftRep.which_side_rep = "left"
                             currentLeftRep.timeup = time.time
-                    
+
                     if right_angle > 150:
                         if currentRightRep is None:
                             currentRightRep = repClass.rep()
@@ -71,30 +79,30 @@ def bicepRendering(max_reps,draw_pose):
                             currentRightRep.timeup = time.time
 
                     if currentLeftRep:
-                            # Have we finsished the curl?
-                            if left_angle < 55:
-                                # Curl is finished, add the rep to workout set
-                                #repStartTime = currentRep.timeup
-                                #currentRep.timeup = time.time - repStartTime
-                                s.left_reps.append(currentLeftRep)
-                                currentLeftRep = None
-                            else:
-                                # In the middle of a curl, check for form issues?
-                                if left_armpit_angle > 35:
-                                    currentLeftRep.add_form_issue("Left elbow too far away from body")
-                    
+                        # Have we finsished the curl?
+                        if left_angle < 55:
+                            # Curl is finished, add the rep to workout set
+                            s.left_reps.append(currentLeftRep)
+                            currentLeftRep = None
+                            Lrep_count += 1
+                        else:
+                            # In the middle of a curl, check for form issues?
+                            if left_armpit_angle > 35:
+                                currentLeftRep.add_form_issue(
+                                    "Left elbow too far away from body")
+
                     if currentRightRep:
-                            # Have we finsished the curl?
-                            if right_angle < 55:
-                                # Curl is finished, add the rep to workout set
-                                #repStartTime = currentRep.timeup
-                                #currentRep.timeup = time.time - repStartTime
-                                s.right_reps.append(currentRightRep)
-                                currentRightRep = None
-                            else:
-                                # In the middle of a curl, check for form issues?
-                                if right_armpit_angle > 35:
-                                    currentRightRep.add_form_issue("Right elbow too far away from body")
+                        # Have we finsished the curl?
+                        if right_angle < 55:
+                            # Curl is finished, add the rep to workout set
+                            s.right_reps.append(currentRightRep)
+                            currentRightRep = None
+                            Rrep_count += 1
+                        else:
+                            # In the middle of a curl, check for form issues?
+                            if right_armpit_angle > 35:
+                                currentRightRep.add_form_issue(
+                                    "Right elbow too far away from body")
 
                     print("\nSet:   ")
                     for rep in s.right_reps:
@@ -102,7 +110,7 @@ def bicepRendering(max_reps,draw_pose):
                     print("----")
                     for rep in s.left_reps:
                         print(rep)
-                    
+
                     if (len(s.left_reps) >= int(max_reps)) and (len(s.right_reps) >= int(max_reps)):
                         with open('bicepcurlform.txt', 'w') as f:
                             f.write("Left Reps:\n")
@@ -131,76 +139,83 @@ def bicepRendering(max_reps,draw_pose):
                                     f.write("\n")
                             f.close()
                         break
-                            
-
 
                 else:
                     # Not tracking, should we be?
                     if count > 0:
-                        cv2.putText(image, "Starting in: " + str(round(count/30)), (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 55, 255), 2)
+                        cv2.putText(image, "Starting in: " + str(round(count/30)),
+                                    (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 55, 255), 2)
                         count -= 1
                     else:
                         tracking = True
-
-
-                #if the angle of the left arm is less than 150 degrees, create a new rep
-                
- 
             except:
                 pass
-            
 
             cv2.imshow('Mediapipe Feed', image)
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
-            
+
         cap.release()
         cv2.destroyAllWindows()
-                       
-def calculate_angle(a,b,c):
-    a = np.array(a) # First
-    b = np.array (b) # Mid
-    C = np.array (c) # End
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    C = np.array(c)  # End
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
+        np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
-    if angle >180.0:
+    if angle > 180.0:
         angle = 360-angle
     return round(angle, 2)
 
+
 def calculate_left_arm(results):
     landmarks = results.pose_landmarks.landmark
-    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-    wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-    #calculate the angle between the shoulders, elbow, and wrists
+    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+             landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+    wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+             landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+    # calculate the angle between the shoulders, elbow, and wrists
     angle = calculate_angle(shoulder, elbow, wrist)
     return angle
+
 
 def calculate_right_arm(result):
     landmarks = result.pose_landmarks.landmark
-    shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-    elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-    wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-    #calculate the angle between the shoulders, elbow, and wrists
+    shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+    elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+             landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+    wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+             landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+    # calculate the angle between the shoulders, elbow, and wrists
     angle = calculate_angle(shoulder, elbow, wrist)
     return angle
 
+
 def calculate_right_armpit(result):
     landmarks = result.pose_landmarks.landmark
-    shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-    elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-    hip =  [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+    shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+    elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+             landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+    hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+           landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
     angle = calculate_angle(shoulder, elbow, hip)
     return abs(angle - 180).round(0)
 
+
 def calculate_left_armpit(result):
     landmarks = result.pose_landmarks.landmark
-    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-    hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x, landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+             landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+    hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+           landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
     angle = calculate_angle(shoulder, elbow, hip)
     return abs(angle - 180).round(0)
-    
-      
-            
-        
--- Lift Check Application/FormScripts/squatForm.py
+++ Lift Check Application/FormScripts/squatForm.py
@@ -17,7 +17,8 @@ from FormScripts import workoutSetClass
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-def squat_render(max_reps,draw_pose):
+
+def squat_render(max_reps, draw_pose):
     s = workoutSetClass.workout_set()
     rep_count = 0
     count = 0
@@ -40,113 +41,125 @@ def squat_render(max_reps,draw_pose):
             # Recolor back to BGR
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            # Render detections 
+            # Render detections
             if draw_pose:
                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                        mp_drawing.DrawingSpec(
-                                            color=(245, 117, 66), thickness=2, circle_radius=2),
-                                        mp_drawing.DrawingSpec(
-                                            color=(245, 66, 230), thickness=2, circle_radius=2)
-                                        )
-            
-            
-            #find the angle of the elbows and the angle of the shoulders
+                                          mp_drawing.DrawingSpec(
+                                              color=(245, 117, 66), thickness=2, circle_radius=2),
+                                          mp_drawing.DrawingSpec(
+                                              color=(245, 66, 230), thickness=2, circle_radius=2)
+                                          )
+
+            # find the angle of the elbows and the angle of the shoulders
             try:
 
                 left_knee_angle = calculate_left_knee_angle(results)
                 left_hip_angle = calculate_left_hip_angle(results)
-                #display the angle on the screen
+                # display the angle on the screen
 
-                cv2.rectangle(image, (0,0), (280,100), (0,0,0), -1)
+                cv2.rectangle(image, (0, 0), (280, 100), (0, 0, 0), -1)
 
-                cv2.putText(image, "Count: " + str(rep_count), (15, 82), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
-                cv2.putText(image, "Left Knee: " + str(left_knee_angle), (15, 35), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
-                cv2.putText(image, "Left Hip Angle: " + str(left_hip_angle), (15, 60), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
-                
+                cv2.putText(image, "Count: " + str(rep_count), (15, 82),
+                            cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+                cv2.putText(image, "Left Knee: " + str(left_knee_angle),
+                            (15, 35), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+                cv2.putText(image, "Left Hip Angle: " + str(left_hip_angle),
+                            (15, 60), cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 55, 255), 2)
+
                 # Are we tracking yet?
                 if tracking:
                     if current_rep is None and left_knee_angle > 160:
-                        # start = time.perf_counter()
                         current_rep = repClass.rep()
                     if current_rep:
                         if (current_rep.min_angle > left_knee_angle):
                             current_rep.min_angle = left_knee_angle
                         if (left_knee_angle - 15 > current_rep.min_angle):
                             if current_rep.min_angle >= 90:
-                                current_rep.add_form_issue("Not to depth (90 degrees or below)")
-                                thread = Thread(target = say_issue, args = ('./parallelissue.mp3',))
+                                current_rep.add_form_issue(
+                                    "Not to depth (90 degrees or below)")
+                                thread = Thread(target=say_issue, args=(
+                                    './parallelissue.mp3',))
                                 thread.start()
                             rep_count += 1
-                            #end = time.perf_counter()
-                            #current_rep.timeup = end-start
-                            #print(current_rep.timeup)
                             s.left_reps.append(current_rep)
                             current_rep = None
 
-                    if (len(s.left_reps) >= int(max_reps)) :
+                    if (len(s.left_reps) >= int(max_reps)):
                         with open('squatForm.txt', 'w') as f:
                             f.write("Reps:\n")
                             for i in range(len(s.left_reps)):
                                 if len(s.left_reps[i].get_form_issues()) == 0:
-                                    f.write("Rep #" + str(i+1)+": Perfect! Depth Angle: " + str(s.left_reps[i].min_angle))
+                                    f.write(
+                                        "Rep #" + str(i+1)+": Perfect! Depth Angle: " + str(s.left_reps[i].min_angle))
                                     f.write("\n")
                                 else:
                                     tempStr = ""
                                     for issue in s.left_reps[i].get_form_issues():
                                         tempStr += str(issue)
                                         tempStr += ', '
-                                    f.write("Rep #" + str(i+1)+": " + tempStr + " Depth Angle: "+ str(s.left_reps[i].min_angle))
+                                    f.write("Rep #" + str(i+1)+": " + tempStr +
+                                            " Depth Angle: " + str(s.left_reps[i].min_angle))
                                     f.write("\n")
                             f.close()
                         break
-                
 
                 else:
                     # Not tracking, should we be?
                     if count > 0:
-                        cv2.putText(image, "Starting in: " + str(round(count/30)), (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 55, 255), 2)
+                        cv2.putText(image, "Starting in: " + str(round(count/30)),
+                                    (200, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 55, 255), 2)
                         count -= 1
                     else:
                         tracking = True
 
             except:
                 pass
-            
 
             cv2.imshow('Mediapipe Feed', image)
             if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
-            
+
         cap.release()
         cv2.destroyAllWindows()
 
-def calculate_angle(a,b,c):
-    a = np.array(a) # First
-    b = np.array (b) # Mid
-    C = np.array (c) # End
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    C = np.array(c)  # End
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
+        np.arctan2(a[1]-b[1], a[0]-b[0])
     angle = np.abs(radians*180.0/np.pi)
-    if angle >180.0:
+    if angle > 180.0:
         angle = 360-angle
     return round(angle, 2)
 
+
 def calculate_left_hip_angle(results):
     landmarks = results.pose_landmarks.landmark
-    hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x, landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-    knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
-    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-    #calculate the angle between the shoulders, elbow, and wrists
+    hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+           landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+    knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+            landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+    # calculate the angle between the shoulders, elbow, and wrists
     angle = calculate_angle(hip, knee, shoulder)
     return abs(180-angle).round(0)
 
+
 def calculate_left_knee_angle(results):
     landmarks = results.pose_landmarks.landmark
-    hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x, landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-    knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
-    ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
-    #calculate the angle between the shoulders, elbow, and wrists
+    hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+           landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+    knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+            landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+    ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+             landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+    # calculate the angle between the shoulders, elbow, and wrists
     angle = calculate_angle(hip, knee, ankle)
     return angle.round(0)
+
 
 def say_issue(mp3):
     playsound(mp3)
